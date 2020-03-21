03e0b2e0d550cfe0f083d7d8563b85c2f094a128
Leonard Craft <leonardcraft64@gmail.com>
2020-03-20 23:00:16 -0500

    Add timestamps to error messages

--- parser.js
+++ parser.js
@@ -301,7 +301,7 @@ exports.parse =
 				}
 				break;
 			case "error": //this protocol is triggered whenever Showdown's errorReply() function is called
-				console.log("Error message from Showdown: " + spl[2]);
+				console.log(new Date().toLocaleString() + ": Error message from Showdown: " + spl[2]);
 				break;
 			case "win":
 				//this.bestOfThree.wins.push(spl[2]);
