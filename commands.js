f32b822e3b3ea54b5659232140856a30def4cad6
Leonard Craft <leonardcraft64@gmail.com>
2020-08-30 10:53:41 -0500

    Switch quagsire image host

--- commands.js
+++ commands.js
@@ -726,7 +726,7 @@ exports.commands =
         		<img src = "https://play.pokemonshowdown.com/sprites/ani/quagsire.gif" width="48" height="77" style="transform: scaleX(-1); display: block;"> \
     		</div> \
 			<div style = "display: inline-block;"> \
-				<a href = "https://www.youtube.com/watch?v=buc64u6Q_oA"><img src = "https://im6.ezgif.com/tmp/ezgif-6-8c83613b10b9.gif" width="360" height="202"></a> \
+				<a href = "https://www.youtube.com/watch?v=buc64u6Q_oA"><img src = "https://cdn.discordapp.com/attachments/656292565242347520/749657145305202748/acquirethesire.gif" width="360" height="202"></a> \
 			</div> \
 			<div style = "display: inline-block; vertical-align: 50%; padding-left: 10px;"> \
 				<img src = "https://play.pokemonshowdown.com/sprites/ani/quagsire.gif" width="48" height="77" style="display: block; padding-bottom: 20px;"> \
