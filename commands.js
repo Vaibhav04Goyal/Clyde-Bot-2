c43263d337b5a7f74506091989dc34cc7e996610
Leonard Craft <leonardcraft64@gmail.com>
2020-09-11 16:46:01 -0500

    This is what I get

--- commands.js
+++ commands.js
@@ -667,10 +667,12 @@ exports.commands =
 	objectively: function(arg, by, room)
 	{
 		let text = 
-		'<p>Something is "objective" when it is true independently of personal feelings or opinions, instead based on hard facts. For example, Flamethrower objectively has higher accuracy than Fire Blast, and Fire Blast objectively has a higher Base Power than Flamethrower.</p> \
-		<p>Subjective refers to personal preferences, opinions, or feelings. Anything subjective is subject to interpretation. For example, you might think Flamethrower is better than Fire Blast, but another player might think Fire Blast is better; the opinion is subjective.</p> \
-		<p>That\'s not to say opinions are bad! It\'s also ok to put forth reasoning into your opinions and defend them. It\'s not correct, however, to say some opinion you have is objectively true.<p> \
-		';
+		"Something is \"objective\" when it is true independently of personal feelings or opinions, instead based on hard facts. For example, Flamethrower objectively has higher accuracy than Fire Blast, and Fire Blast objectively has a higher Base Power than Flamethrower. \
+		<br><br> \
+		Subjective refers to personal preferences, opinions, or feelings. Anything subjective is subject to interpretation. For example, you might think Flamethrower is better than Fire Blast, but another player might think Fire Blast is better; the opinion is subjective. \
+		<br><br> \
+		That's not to say opinions are bad! It's also ok to put forth reasoning into your opinions and defend them. It's not correct, however, to say some opinion you have is objectively true. \
+		";
 		this.say(room, "/addhtmlbox " + text);
 	},
 
@@ -727,7 +729,7 @@ exports.commands =
 		'<marquee scrollamount = "15">';
 		for (i = 0; i < 13; i++)
 		{
-			text += '	<img src = "https://play.pokemonshowdown.com/sprites/ani/diglett.gif" class = "fa fa-spin" width = "43" height = "35"';
+			text += '	<img src = "https://play.pokemonshowdown.com/sprites/ani/diglett.gif" class = "fa fa-spin" width = "43" height = "35">';
 		}
 		//D I G L E T T in emoji
 		text +=
@@ -760,7 +762,7 @@ exports.commands =
 		';
 		for (i = 0; i < 13; i++)
 		{
-			text += '	<img src = "https://play.pokemonshowdown.com/sprites/ani-back/diglett.gif" class = "fa fa-spin" width = "43" height = "35"';
+			text += '	<img src = "https://play.pokemonshowdown.com/sprites/ani-back/diglett.gif" class = "fa fa-spin" width = "43" height = "35">';
 		}
 		text +=
 		'</marquee>';
