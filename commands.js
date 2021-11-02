08fea581209388c233691963afcf006e84af557b
Leonard Craft III <lcraft@jwcc.edu>
2021-11-02 08:58:36 -0500

    Add yet another excellent joke

--- commands.js
+++ commands.js
@@ -559,6 +559,7 @@ exports.commands =
 			"How do you help a female sibling? Assister!",
 			"A guy came into my office claiming he was able to turn people into windmills. I immediately became a huge fan!",
 			"Why did the ghost go to the bar? To get some booze!",
+			"I'm super upset. Somebody glued my deck of cards together, and I just don't know how to deal with it!",
 		];
 
 		let jokeNum = arg === "latest" ? jokeList.length - 1 : parseInt(arg);
